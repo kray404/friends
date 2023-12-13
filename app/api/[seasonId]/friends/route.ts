cdd09c20e669bc50177e1df4e0dc4b4c8ff715c7
shv-gitman <mikael404@pm.me>
2023-12-13 18:09:57 +0100

    Changed Request

--- app/api/[seasonId]/friends/route.ts
+++ app/api/[seasonId]/friends/route.ts
@@ -1,15 +1,11 @@
 import prisma from "@/lib/prisma";
-import type { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
 interface MyParams {
   seasonId: string;
 }
 
-export async function GET(
-  request: NextApiRequest,
-  { params }: { params: MyParams }
-) {
+export async function GET(request: Request, { params }: { params: MyParams }) {
   const seasonId = params.seasonId;
 
   const friends = await prisma.friend.findMany({
