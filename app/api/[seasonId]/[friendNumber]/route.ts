56ded07c3fc0fccf77102b3ec03a56e7df7c4eba
shv-gitman <mikael404@pm.me>
2023-12-16 22:46:19 +0100

    Added better api/seasonId/number response

--- app/api/[seasonId]/[friendNumber]/route.ts
+++ app/api/[seasonId]/[friendNumber]/route.ts
@@ -6,6 +6,16 @@ interface MyParams {
   friendNumber: string;
 }
 
+// Function to format the seasonId into a readable format
+function formatSeasonId(seasonId: string): string {
+  // Split the seasonId into parts
+  const parts = seasonId.split("_");
+  // Capitalize the first letter of each part and join them back with a space
+  return parts
+    .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+    .join(" ");
+}
+
 export async function GET(request: Request, { params }: { params: MyParams }) {
   const { friendNumber, seasonId } = params;
 
@@ -19,17 +29,20 @@ export async function GET(request: Request, { params }: { params: MyParams }) {
 
   // Check if the friend exists
   if (friend) {
+    // Format the seasonId
+    const formattedSeasonId = formatSeasonId(seasonId);
+
     // Format the response with the friend's details
-    const responseMessage = `Friend Number ${friend.number} is ${friend.name} (${friend.description}). Go follow them! ${friend.twitchLink}`;
+    const responseMessage = `Friend Number ${friend.number} for ${formattedSeasonId} is ${friend.name}. Go follow them! ${friend.twitchLink}`;
 
     // Return the formatted message
-    return new Response(responseMessage, {
+    return new NextResponse(responseMessage, {
       status: 200,
       statusText: "OK",
     });
   } else {
     // Return an appropriate response if the friend does not exist
-    return new Response("Friend not found", {
+    return new NextResponse("Friend not found", {
       status: 404,
       statusText: "Not Found",
     });
